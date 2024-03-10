180de2a5c44f84fa8396e983ed23df007b7843f6
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-03-10 11:41:25 +0330

    add upload image

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const fs = require('fs');
 
 const app = express();
 
-// تنظیمات آپلود با Multer
 const storage = multer.diskStorage({
-    destination: './uploads/',
+    destination: './public/uploads/',
     filename: function(req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
@@ -17,10 +16,8 @@ const upload = multer({
     storage: storage
 }).single('myImage');
 
-// استفاده از موتور قالب EJS برای نمایش عکس‌ها
 app.set('view engine', 'ejs');
 
-// آپلود عکس
 app.post('/upload', (req, res) => {
     upload(req, res, (err) => {
         if (err) {
@@ -39,30 +36,26 @@ app.post('/upload', (req, res) => {
     });
 });
 
-// نمایش عکس‌های آپلود شده
 app.get('/images', (req, res) => {
-    // خواندن لیست فایل‌های موجود در پوشه uploads
-    fs.readdir('./uploads', (err, files) => {
+    fs.readdir('./public/uploads', (err, files) => {
         if (err) {
             console.error('Error reading directory:', err);
             res.status(500).send('Internal Server Error');
             return;
         }
 
-        // فیلتر کردن فقط فایل‌های تصویری
         const imageFiles = files.filter(file => {
             const extname = path.extname(file);
             return ['.jpg', '.jpeg', '.png', '.gif'].includes(extname.toLowerCase());
         });
 
-        // ارسال لیست عکس‌های آپلود شده به قالب برای نمایش
         res.render('images', { images: imageFiles });
     });
 });
 
-// صفحه اصلی
 app.get('/', (req, res) => res.render('index', { msg: null }));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.use('/public/uploads', express.static('./public/uploads'));
